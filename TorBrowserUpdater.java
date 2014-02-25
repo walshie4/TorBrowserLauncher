64a9362e371ffb104680713a22840d546115eb9e
Adam Walsh <adamwalsh14@gmail.com>
2014-02-25 11:11:24 -0500

    Inital commit of Java re-write

---
+++ TorBrowserUpdater.java
@@ -0,0 +1,21 @@
+/**
+ * TorBrowserUpdater.java
+ *
+ * A simple program that checks for a Tor Browser Bundle
+ * install, then checks torproject.org for a new release
+ * and if there is one it will download it, delete the old
+ * one and verify the signature of the new release.
+ */
+
+/**
+ * Written by: Adam Walsh
+ * Written on: 2/25/14
+ * Maintained at: https://github.com/walshie4/TorBrowserUpdater
+ */
+
+public class TorBrowserUpdater {
+
+    public static void main(String[] args) {
+        //do stuff
+    }
+}
