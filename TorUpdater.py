4f5ad857cfdb16fc0c0850f1fb5baf6ce86d5881
Adam Walsh <adamwalsh14@gmail.com>
2014-04-03 14:59:23 -0400

    Added inital skeleton of TorUpdater.py

--- TorUpdater.py
+++ TorUpdater.py
@@ -2,3 +2,25 @@
 #Written by: Adam Walsh
 #Written on 4/2/14
 
+class TBBUpdater:
+    def __init__:
+        self.currentVersion = getCurrentVersion()
+        self.installedVersion = getInstalledVersion()
+
+    def detectLocalInstall:
+#look in default location to find install, if not found ask user if a custom
+#location install exists
+
+    def getInstalledVersion(path):
+#get current version using path to local install
+
+    def getCurrentVersion:
+#get newest version from torproject.org
+
+if __name__=="__main__":
+    updater = TBBUpdater()
+    if updater.upToDate():
+        #no need to update, launch bundle
+    else
+        updater.update()
+    updater.launchTBB()
